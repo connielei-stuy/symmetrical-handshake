9228c21e9e879a1d925f28e3669c9a2eac29b014
clei1 <clei1@stuy.edu>
2017-12-13 21:31:27 -0500

    working handshake

---
+++ pipe_networking.c
@@ -0,0 +1,58 @@
+#include "pipe_networking.h"
+
+/*=========================
+  server_handshake
+  args: int * to_client
+
+  Performs the client side pipe 3 way handshake.
+  Sets *to_client to the file descriptor to the downstream pipe.
+
+  returns the file descriptor for the upstream pipe.
+  =========================*/
+int server_handshake(int *to_client) {
+  mkfifo("wkp", 0644); //creates FIFO (well known pipe)
+  int upstream = open("wkp", O_RDONLY); //opens FIFO and waits for connection
+  
+  char hand[HANDSHAKE_BUFFER_SIZE];
+  read(upstream, hand, sizeof(hand)); //reads client's private FIFO name
+  remove("wkp"); //removes well known pipe so another client doesn't connect
+   
+  int downstream = open(hand, O_WRONLY); //connects to client's private FIFO
+  write(downstream, ACK, sizeof(ACK)); //sends initial message after connecting
+  read(upstream, hand, sizeof(hand));
+
+  *to_client = downstream; //add comment here to remind self about addresses, pointers
+  return upstream;
+}
+
+
+/*=========================
+  client_handshake
+  args: int * to_server
+
+  Performs the client side pipe 3 way handshake.
+  Sets *to_server to the file descriptor for the upstream pipe.
+
+  returns the file descriptor for the downstream pipe.
+  =========================*/
+int client_handshake(int *to_server) {
+  char hand[HANDSHAKE_BUFFER_SIZE];
+  //itoa(getpid(), hand, 10);
+
+  sprintf(hand, "%d", getpid());
+  mkfifo(hand, 0644); //creates private FIFO
+
+  int upstream = open("wkp", O_WRONLY); //connects to server's WKP
+  write(upstream, hand, sizeof(hand)); //sends private FIFO name to server
+
+  int downstream = open(hand, O_RDONLY); //opens private FIFO
+
+  char ack[sizeof(ACK)];
+  read(downstream, ack, sizeof(ack)); //waits for server acknowledgement message
+  remove(hand);
+  write(upstream, ACK, sizeof(ACK));
+
+  *to_server = upstream;
+
+  return downstream;
+}
